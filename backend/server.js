6fa81a49802e12a9f39b01749a606a9b9b42b928
m2lovric <m2lovric@gmail.com>
2022-07-04 00:16:48 +0200

    Connect to mongo

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 require('dotenv').config();
 const routes = require('./routes/workouts.js');
+const mongoose = require('mongoose');
 
 const port = process.env.PORT;
 const app = express();
@@ -14,6 +15,13 @@ app.use((req, res, next) => {
 
 app.use('/api/workouts', routes);
 
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
-});
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`Connected to db & Server running on port ${port}`);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
