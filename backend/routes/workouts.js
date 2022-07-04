9f96f29dbe4cf112652ee6325f61bf85682a8b47
m2lovric <m2lovric@gmail.com>
2022-07-04 12:45:49 +0200

    Add model and update post method

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -1,5 +1,5 @@
 const express = require('express');
-
+const Workout = require('../models/workoutModel');
 const router = express.Router();
 
 router.get('/', (req, res) => {
@@ -10,8 +10,14 @@ router.get('/:id', (req, res) => {
   res.json({ mssg: 'get a single workout' });
 });
 
-router.post('/', (req, res) => {
-  res.json({ mssg: 'post a new workout' });
+router.post('/', async (req, res) => {
+  const { title, reps, load } = req.body;
+  try {
+    const workout = await Workout.create({ title, reps, load });
+    res.status(200).json(workout);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
 });
 
 router.delete('/:id', (req, res) => {
